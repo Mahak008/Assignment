ec278a0e64e4749dee7b160ed89a1c2e3e8135e9
Mahak <mahakgarg8002@gmail.com>
2024-10-10 14:48:21 +0530

    update

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -11,7 +11,9 @@ const Card = () => {
 
   const handleNext = () => {
     if (currentIndex < cardData.length - 1) {
-      setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, cardData.length - 1));
+      setCurrentIndex((prevIndex) =>
+        Math.min(prevIndex + 1, cardData.length - 1)
+      );
     }
   };
 
@@ -33,7 +35,10 @@ const Card = () => {
   return (
     <div className="flex flex-col-reverse md:flex-row items-center justify-center w-full h-screen px-4">
       {/* Main Card Section */}
-      <div className="relative flex flex-col border border-[#db9125] rounded-lg shadow-lg custom-gradient" style={{ width: "100%", maxWidth: "550px", height: "320px" }}>
+      <div
+        className="relative flex flex-col border border-[#db9125] rounded-lg shadow-lg custom-gradient"
+        style={{ width: "100%", maxWidth: "550px", height: "320px" }}
+      >
         <div className="absolute top-4 right-4 bg-green-500 text-white font-semibold px-2 py-1 rounded z-10">
           {cardData[currentIndex].hike}
         </div>
@@ -63,37 +68,71 @@ const Card = () => {
         </div>
 
         <div className="p4">
-          <p className="text-2xl text-white px-4">{cardData[currentIndex].company}</p>
+          <p className="text-2xl text-white px-4">
+            {cardData[currentIndex].company}
+          </p>
         </div>
       </div>
 
       {/* Frame Section */}
-      <div className="flex flex-row ml-4 space-x-2">
-        <button onClick={handlePrev} className="text-black font-bold text-xl px-2 py-1">{"←"}</button>
-        <div className="flex flex-row space-x-2 overflow-hidden" style={{ width: "400px" }}>
-          {/* Display current visible cards */}
-          {currentCards.map((item, index) => {
-            const cardIndex = start + index; // Calculate actual index in cardData
-            const isActive = cardIndex === currentIndex; // Check if this card is the active one
-            return (
-              <div
-                key={cardIndex}
-                className={`w-20 h-20 border rounded-lg ${isActive ? "border-[#db9125]" : "border-gray-400"} ${isActive ? '' : 'filter blur-sm'}`}
-                style={{ position: "relative" }}
-                onClick={() => handleImageClick(index)} // Add click handler
-              >
-                <Image
-                  src={item.imageUrl}
-                  alt={item.name}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg cursor-pointer" 
-                />
-              </div>
-            );
-          })}
+      <div className="flex flex-col items-start md:ml-10 mt-6 md:mt-0 px-4 mx-4">
+        <h2 className="text-2xl md:text-3xl font-bold text-black text-center md:text-left">
+          Real Stories, Real Success
+        </h2>
+
+        <p className="text-lg mt-2 mb-4 text-gray-400 text-center md:text-left">
+          Discover what our learners say about us
+        </p>
+
+        <div className="flex flex-row ml-4">
+          <button
+            onClick={handlePrev}
+            className="text-black font-bold text-xl px-2 py-1"
+            style={{ visibility: currentIndex > 0 ? "visible" : "hidden" }}
+          >
+            {"←"}
+          </button>
+
+          <div
+            className="flex flex-row space-x-2 overflow-hidden"
+            style={{ width: "400px" }}
+          >
+            {/* Display current visible cards */}
+            {currentCards.map((item, index) => {
+              const cardIndex = start + index; // Calculate actual index in cardData
+              const isActive = cardIndex === currentIndex; // Check if this card is the active one
+              return (
+                <div
+                  key={cardIndex}
+                  className={`w-20 h-20 border rounded-lg ${
+                    isActive ? "border-[#db9125]" : "border-gray-400"
+                  } ${isActive ? "" : "filter blur-sm"}`}
+                  style={{ position: "relative" }}
+                  onClick={() => handleImageClick(index)} // Add click handler
+                >
+                  <Image
+                    src={item.imageUrl}
+                    alt={item.name}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-lg cursor-pointer"
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          <button
+            onClick={handleNext}
+            className="text-black font-bold text-xl px-2 py-1"
+            style={{
+              visibility:
+                currentIndex < cardData.length - 1 ? "visible" : "hidden",
+            }}
+          >
+            {"→"}
+          </button>
         </div>
-        <button onClick={handleNext} className="text-black font-bold text-xl px-2 py-1">{"→"}</button>
       </div>
     </div>
   );
